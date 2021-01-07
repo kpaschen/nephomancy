6f2e014716cecd566b354af6fd7c93fb683949e6
Developer <kathrin.paschen@gmail.com>
2021-01-07 15:26:14 +0100

    init command. also added command superclass

--- gcloud/command/cost.go
+++ gcloud/command/cost.go
@@ -1,21 +1,46 @@
 package command
 
 import (
-	"database/sql"
-	"flag"
 	"fmt"
 	"log"
+	"strings"
 	"nephomancy/gcloud/assets"
 	pricing "nephomancy/gcloud/pricing"
 	"nephomancy/gcloud/cache"
 )
 
 type CostCommand struct {
-	// Probably want some credentials and config here.
+	Command
 }
 
 func (*CostCommand) Help() string {
-	return "Prints cost estimates for your gcloud setup."
+	helpText := `
+	Usage: nephomancy gcloud cost [options]
+
+	Print a summary of resources and their estimated costs per month.
+	Resources include compute (VMs, licenses); network (IP addresses,
+	load balancers, egress); and storage (disks, images, cloud storage).
+	So far, resources such as BigTable, Spanner etc are not included.
+
+	Costs are estimated based on SKU pricing info published by Google.
+
+	I make no guarantee that the estimate will be correct; you can cross-check with
+	your project's billing console as well as with the Google Pricing Calculator.
+
+	Options:
+	  --workingdir=path  Optional: directory under which the cached data can be found.
+	                     The cost command needs this in order to locate the sku cache 
+			     database. If not set, defaults to current directory. The sku database
+			     will be looked for in $workingdir/.nephomancy/gcloud/data/sku-dache.db.
+			     If you do not have a cache db yet, create one using nephomancy gcloud init.
+
+	  --project=PROJECT  ID of a gcloud project. The user you are authenticating as must have
+	                     access to this project. The billing, compute, asset, and monitoring APIs
+			     must be enabled for this project. Costs reported will be for assets associated
+			     with this project as well as for assets whose costs are only available at
+			     the organization or billing account level.
+`
+	return strings.TrimSpace(helpText)
 }
 
 func (*CostCommand) Synopsis() string {
@@ -23,22 +48,27 @@ func (*CostCommand) Synopsis() string {
 }
 
 // Run this with
-// nephomancy gcloud cost --project=binderhub-test-275512 --dbfile=gcloud/cmds/sku-cache.db
-func (*CostCommand) Run(args []string) int {
-	fs := flag.NewFlagSet("gcloudCost", flag.ExitOnError)
-	var project, dbFile string
-	fs.StringVar(&project, "project", "", "project name")
-	fs.StringVar(&dbFile, "dbfile", "sku-cache.db", "sku cache db file")
+// nephomancy gcloud cost --project=binderhub-test-275512
+func (c *CostCommand) Run(args []string) int {
+	fs := c.Command.defaultFlagSet("gcloudCost")
 	fs.Parse(args)
 
-	db, _ := sql.Open("sqlite3", dbFile)
-	defer db.Close()
+	db, err := c.DbHandle()
+	if err != nil {
+		log.Fatalf("Could not open sku cache db: %v\nTry creating it with nephomancy gcloud init. If that does not help, try removing the .nephomancy/gcloud/data directory and then run init again.\n")
+	}
+	defer c.CloseDb()
+
+	project := c.Command.Project
+	if project == "" {
+		log.Fatalf("Need a project ID. You can see the IDs on the Gcloud console.\n")
+	}
 
 	projectPath := fmt.Sprintf("projects/%s", project)
 
 	assets, err := assets.ListAssetsForProject(projectPath)
 	if err != nil {
-		log.Fatalf("listing assets: %v", err)
+		log.Fatalf("Listing assets failed: %v", err)
 	}
 	for _, a := range assets {
 		if a.AssetType == "" {
@@ -50,14 +80,14 @@ func (*CostCommand) Run(args []string) int {
 			continue
 		}
 		if len(prices) == 0 {
-			log.Printf("zero prices for asset %s\n", a.Name)
+			log.Printf("Zero prices for asset %s\n", a.Name)
 			continue
 		}
 		err = pricing.CostRange(&a, prices)
 		if err != nil {
 			log.Fatalf("Failed to get price range: %v\n", err)
 		}
-		fmt.Printf("prices for asset %+v: %+v\n", a, prices)
+		// fmt.Printf("Prices for asset %+v: %+v\n", a, prices)
 	}
 	return 0
 }
--- go.mod
+++ go.mod
@@ -3,22 +3,7 @@ module nephomancy
 go 1.15
 
 require (
-	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/gogo/protobuf v1.3.1 // indirect
-	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
-	github.com/iancoleman/strcase v0.1.2 // indirect
-	github.com/lyft/protoc-gen-star v0.5.2 // indirect
+	github.com/mattn/go-sqlite3 v1.14.6
 	github.com/mitchellh/cli v1.1.2
-	github.com/nbutton23/zxcvbn-go v0.0.0-20201221231540-e56b841a3c88 // indirect
-	nephomancy/gcloud/assets v0.0.0-00010101000000-000000000000
-	nephomancy/gcloud/cache v0.0.0-00010101000000-000000000000
-	nephomancy/gcloud/command v0.0.0-00010101000000-000000000000
+	google.golang.org/api v0.36.0
 )
-
-replace nephomancy/gcloud/assets => ./gcloud/assets
-
-replace nephomancy/gcloud/cache => ./gcloud/cache
-
-replace nephomancy/gcloud/pricing => ./gcloud/pricing
-
-replace nephomancy/gcloud/command => ./gcloud/command
