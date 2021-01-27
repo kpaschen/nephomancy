08665e854750dcbe4f3841908ab36e063e3638dd
Developer <kathrin.paschen@gmail.com>
2021-01-27 15:50:02 +0100

    basics of querying monitoring api

---
+++ gcloud/assets/monitoring.go
@@ -0,0 +1,107 @@
+package assets
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"cloud.google.com/go/monitoring/apiv3"
+//	metricpb "google.golang.org/genproto/googleapis/api/metric"
+	monitoringpb "google.golang.org/genproto/googleapis/monitoring/v3"
+	"google.golang.org/api/iterator"
+	timestamppb "github.com/golang/protobuf/ptypes/timestamp"
+)
+
+// code is here: https://godoc.org/cloud.google.com/go/monitoring/apiv3#pkg-files
+
+// e.g. metricType=networking.googleapis.com/vm_flow/egress_bytes_count
+// resource.type=gce_instance
+// Probably no need for this, can just get the timeseries directly.
+func getMetricDescriptors(project string, metricType string, resourceType string) error {
+	ctx := context.Background()
+	client, err := monitoring.NewMetricClient(ctx)
+	if err != nil {
+		return err
+	}
+	nextPageToken := ""
+	// filter := fmt.Sprintf(`metric.type=starts_with("%s")`, "networking.googleapis.com")
+	filter := fmt.Sprintf(`metric.type=starts_with("%s") AND resource.type=starts_with("%s")`,
+	metricType, resourceType)
+	for {
+		req := &monitoringpb.ListMetricDescriptorsRequest{
+			Name: project,
+			Filter: filter,
+			PageSize: 100,
+			PageToken: nextPageToken,
+		}
+		it := client.ListMetricDescriptors(ctx, req)
+		for {
+			resp, err := it.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				return err
+			}
+			fmt.Printf("resp: %+v\n", resp)
+		}
+		if it.PageInfo().Token == "" {
+			break
+		}
+		nextPageToken = it.PageInfo().Token
+	}
+	return nil
+}
+
+// This gets the most recent value of the given time series, averaged
+// over all instances.
+// TODO: finish debugging this once there is monitoring data.
+func getTimeSeries(project string, metricType string, resourceType string) error {
+	ctx := context.Background()
+	client, err := monitoring.NewMetricClient(ctx)
+	if err != nil {
+		return err
+	}
+	now := time.Now().Unix()
+	interval := &monitoringpb.TimeInterval{
+		EndTime: &timestamppb.Timestamp{
+			Seconds: now,
+		},
+		StartTime: &timestamppb.Timestamp{
+			Seconds: now-60,
+		},
+	}
+	filter := fmt.Sprintf(`metric.type=starts_with("%s") AND resource.type=starts_with("%s")`,
+	metricType, resourceType)
+	req := &monitoringpb.ListTimeSeriesRequest{
+		Name: project,
+		Filter: filter,
+		Interval: interval,
+		Aggregation: &monitoringpb.Aggregation{
+			CrossSeriesReducer: monitoringpb.Aggregation_REDUCE_SUM,
+		},
+	}
+	it := client.ListTimeSeries(ctx, req)
+	for {
+		resp, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Printf("ts: %+v\n", resp)
+	}
+	return nil
+}
+
+func ListMetrics(project string) error {
+	err := getMetricDescriptors(project,
+	"networking.googleapis.com/vm_flow/egress_bytes_count", "gce_instance")
+	if err != nil {
+		return err
+	}
+	err = getTimeSeries(project,
+	"networking.googleapis.com/vm_flow/egress_bytes_count", "gce_instance")
+	return nil
+}
---
+++ gcloud/command/services.go
@@ -0,0 +1,56 @@
+package command
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"nephomancy/gcloud/assets"
+)
+
+type ServicesCommand struct {
+	Command
+}
+
+func (*ServicesCommand) Help() string {
+	helpText := `
+	Usage: nephomancy gcloud services [options]
+
+	Print a summary of services in use by a project.
+
+	Options:
+	  --project=PROJECT  ID of a gcloud project. The user you are authenticating as must have
+	                     access to this project. The billing, compute, asset, and monitoring APIs
+			     must be enabled for this project.`
+	return strings.TrimSpace(helpText)
+}
+
+func (*ServicesCommand) Synopsis() string {
+	return "Prints services list for a gcloud project."
+}
+
+// Run this with
+// nephomancy gcloud services --project=binderhub-test-275512
+func (c *ServicesCommand) Run(args []string) int {
+	fs := c.Command.defaultFlagSet("gcloudServices")
+	fs.Parse(args)
+
+	project := c.Command.Project
+	if project == "" {
+		log.Fatalf("Need a project ID. You can see the IDs on the GCloud console.\n")
+	}
+
+	projectPath := fmt.Sprintf("projects/%s", project)
+
+	err := assets.ListServices(projectPath)
+	if err != nil {
+		log.Fatalf("Failed to get services: %v", err)
+	}
+
+	// err = assets.ListMetrics(projectPath, `metric.type=starts_with("compute.googleapis.com")`)
+	err = assets.ListMetrics(projectPath)
+	if err != nil {
+		log.Fatalf("Failed to get metrics: %v", err)
+	}
+
+	return 0
+}
